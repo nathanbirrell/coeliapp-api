ffdbb8b5a12244fb72d803eb8f43b5968e5ecef9
Seamus Minogue <sminogue@yahoo.com>
2016-12-01 20:17:44 -0500

    converted application to express
    Converted application to express and added true rest image upload
    endpoint

--- index.js
+++
@@ -1,8 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
-
-var handle = {};
-handle["/ocr"] = requestHandlers.performOcr;
-
-server.start(router.route, handle);
