9b6e52b0ee9f0f3ce08d6d97bdab8ed0db70c209
Nathan Birrell <nathanbirrell@gmail.com>
2018-02-12 13:38:15 +1100

    Add more parsing rules

--- helpers/Scan.js
+++ helpers/Scan.js
@@ -1,23 +1,36 @@
 const WORDS_TO_CUT = [
+  'ingredients: ',
   'ingredients',
-  'ingredients:',
 ];
 
 function parseIngredients(rawText) {
   let cleanText = '';
+
   // Replace newlines with space
   cleanText = rawText.replace(/\n/g, ' ');
+
   // Replace multiple consecutive spaces with single space
   cleanText = cleanText.replace(/\s+/g, ' ');
+
+  // Trim full-stops
+  cleanText = cleanText.replace(/\./, '');
+
   // Cut out useless words
   WORDS_TO_CUT.forEach((word) => {
     cleanText = cleanText.replace(new RegExp(word, 'ig'), '');
   });
+
+  // TODO: trim between brackets?
+
   // Trim whitespace at start/end
   cleanText = cleanText.trim();
+
   // Split into comma-delimited array
-  var array = cleanText.split(', ');
-  return array;
+  const array = cleanText.split(', ');
+
+  // TODO: trim each item in array?
+
+  return array
 }
 
 class Scan {
