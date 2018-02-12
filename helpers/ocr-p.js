dff9c56e98720490c3e13a8e7274e365375a017f
Nathan Birrell <nathanbirrell@gmail.com>
2018-02-12 12:16:38 +1100

    Minor cleaning up and testing

--- helpers/ocr-p.js
+++ helpers/ocr-p.js
@@ -1,43 +1,11 @@
-var Tesseract = require('tesseract.js'),
-  fs = require('fs'),
-  config = require('config'),
-  tessocr = require('ntesseract'),
-  Q = require("q");
-
-
+var fs = require('fs');
+var config = require('config');
+var tesseract = require('ntesseract');
+var Q = require("q");
 
 function run(filename) {
-  var useTesseract = config.get('tesseract-ocr.enabled');
-
-  if (useTesseract) {
-    return tesseractCA(filename);
-  } else {
-    return tesseractjs(filename);
-  }
-
-}
-
-function tesseractjs(filename) {
-  var deferred = Q.defer();
-  Tesseract.recognize(filename)
-    .progress(function (p) {
-      //Status messages
-    })
-    .catch(function (err) {
-      deferred.reject(err);
-    })
-    .then(function (result) {
-      deferred.resolve(result.text);
-    });
-
-  return deferred.promise;
-
-}
-
-function tesseractCA(filename) {
-
   return new Q.Promise(function (resolve, reject) {
-    tessocr.process(filename, function (err, text) {
+    tesseract.process(filename, function (err, text) {
       if (err) {
         reject(err);
       } else {
@@ -45,7 +13,6 @@ function tesseractCA(filename) {
       }
     });
   });
-
 }
 
 exports.run = run;
